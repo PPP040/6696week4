83ff430ecb0336eed7a91002c32e92b490d2617b
Patcharapa040 <pphlaynukul@gmail.com>
2024-12-18 10:02:14 +0700

    ex2

---
+++ ex2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+      <!--  -->
+<!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    
+    <style>
+        body{
+            font-family: "K2D", sans-serif;
+            font-weight: 100;
+            font-style: normal;
+            margin-left:100px;
+        }
+    </style>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ตัวอย่าง ภาษา PHP และBootstrap
+    </head>
+    <body>
+        <h1>การเขียนโปรแกรมด้วย PHP <h1>
+        664485040 พัชรภา พลายนุกูล <br>
+        หมู่เรียน 66/96
+
+        <div class="alert alert-success">
+        <strong>สำเร็จแล้ว <a href="#" class="alert-link">ใช้ Botstrap ได้แล้วว
+</div>
+</body>
+        <!--  -->
+        <!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
